d9b877042423d1162473da65fb3cc9c4eec3ddff
Bharat kumar <bksharma1881@gmail.com>
2024-04-17 20:41:44 +0530

    commit

---
+++ models/listing.js
@@ -0,0 +1,61 @@
+// const mongoose=require("mongoose");
+// const Schema=mongoose.Schema;
+
+// const listingSchema= new Schema({
+//     title:{
+//      type:String,
+//      required:true
+//     },
+//     description:String,
+//     image:{
+//         type:String,
+//         // filename: { type: String, required: true },
+//         // url: { type: String, required: true }
+    
+//         // filename: String,
+//         // default: 
+//         //     "https://www.pexels.com/photo/a-close-up-of-pink-roses-in-a-garden-20627179/",
+//         // set: (v)=> 
+//         // v === "" 
+//         // ? "https://www.pexels.com/photo/a-close-up-of-pink-roses-in-a-garden-20627179/"
+//         //  : v,
+//        },
+//     price:Number,
+//     location:String,
+//     country:String
+// });
+
+// const Listing=mongoose.model("Listing", listingSchema);
+
+// module.exports=Listing;
+
+
+
+
+
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
